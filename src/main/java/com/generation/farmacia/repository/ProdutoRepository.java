885765608408048fc074f94bc403e1ec46740856
querenneves <querensantos1@hotmail.com>
2023-04-20 15:09:53 -0300

    Desafio manipulação em tabela produtos realizado

--- src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
+++ src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
@@ -1,5 +1,4 @@
 package com.generation.farmacia.repository;
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +9,4 @@ import com.generation.farmacia.model.Produto;
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 	List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
-
 }
